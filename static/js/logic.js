668fe1646566b3c6fbb2c32058f9767f81530db9
kjanghala <spo.kanika@gmail.com>
2019-10-20 22:21:41 -0700

    color change magnitude

--- static/js/logic.js
+++ static/js/logic.js
@@ -12,18 +12,43 @@ var myMap = L.map("map", {
   
   var newtry = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
   
+    
+  function chooseColor(x) {
+    switch (true) {
+    case (x < 10):
+      return "green";
+    case (x < 20):
+      return "yellow";
+    case (x < 30):
+      return "orange";
+    case (x < 40):
+      return "orangered";
+    case (x < 50):
+      return "red";
+    case (x < 200):
+      return "firebrick";
+    default:
+      return "gray";
+    }
+  };
+  
   d3.json(newtry, function(response) {
   
     console.log(response);
+     
     var num_feats = response.features
     for (var i = 0; i < num_feats.length; i++) {
-         var ph1 = num_feats[i];
-         var location = ph1.geometry
-  
+         var location = num_feats[i].geometry;
+         var magnitude = (num_feats[i].properties.mag)*15;
+      
       if (location) {
-        L.marker([location.coordinates[1], location.coordinates[0]]).addTo(myMap);
+        console.log(magnitude)
+        L.circle([location.coordinates[1], location.coordinates[0]], {
+          color:chooseColor(magnitude),
+          fillOpacity: 0.5,
+          radius: magnitude
+        }).addTo(myMap);
       }
     }
   
   });
-  
